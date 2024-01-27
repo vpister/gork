2c5246689e753bc2c6008981c429771f4d65ca0d
Anna Arpaci-Dusseau <annaad@Annas-MacBook-Pro.local>
2024-01-27 18:33:40 -0500

    fix worldgen

--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -6,22 +6,46 @@ using UnityEngine;
 public class WorldGenerator : MonoBehaviour
 {
 
-    public GameObject tree;
-    public int worldHeight;
-    public int worldWidth;
-    // Start is called before the first frame update
+    public int width = 100;
+    public int height = 100;
+    public float scale = 20f; // Determines the scale of the noise
+    public float landThreshold = 0.5f; // Threshold for land generation
+    public float forestThreshold = 0.6f; // Threshold for forest generation
+
+    public GameObject landTilePrefab;
+    public GameObject waterTilePrefab;
+    public GameObject forestPrefab;
+
     void Start()
     {
-        for(int x = 0; x < worldWidth; x++) {
-            for (int y = 0; y < worldHeight; y++) {
-                Instantiate(tree, new Vector2(x,y) - new Vector2(worldWidth/2, worldHeight/2), Quaternion.identity);
-            }
-        }
+        GenerateWorld();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateWorld()
     {
-        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float perlinValue = Mathf.PerlinNoise(x / scale, y / scale);
+
+                // Create land or water based on the perlin value
+                GameObject tilePrefab = perlinValue > landThreshold ? landTilePrefab : waterTilePrefab;
+                GameObject tile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+
+                tile.transform.parent = this.gameObject.transform;
+
+                // If it's land, check for forest generation
+                if (perlinValue > landThreshold)
+                {
+                    float forestValue = Mathf.PerlinNoise(x / (scale * 2), y / (scale * 2));
+                    if (forestValue > forestThreshold)
+                    {
+                        GameObject t = Instantiate(forestPrefab, new Vector3(x, y), Quaternion.identity, tile.transform);
+                        t.transform.parent = this.transform;
+                    }
+                }
+            }
+        }
     }
 }
